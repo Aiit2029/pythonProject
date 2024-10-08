d179656efbf8376707cf410198f59219753bafda
jinbuhuan <1240906543@qq.com>
2024-10-08 21:20:21 +0800

    Changes

---
+++ mysql知识点/demo_sql1.sql
@@ -0,0 +1,5 @@
+# create table t1(
+#     id int,
+#     age tinyint unsigned
+# )
+insert into t1 values(1,20)
---
+++ mysql知识点/mysql约束.py
@@ -0,0 +1,9 @@
+# not null
+# unique
+# not noll unique  == primary  、
+# 第一个被设置为not noll unique 会自动转换为 primary
+# default
+# 联合 唯一 unique(ip,port)
+# 联合 主键 primary key(ip,port)
+# 表之间的关系
+    # 多对多 出现两外一张表 ，表中存放 别的表的关系 ，多个外键
