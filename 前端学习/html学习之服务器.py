d835ee6cb09ed1a35d984ec4c0a38fc7dadfa9cf
jinbuhuan <1240906543@qq.com>
2024-10-11 19:29:50 +0800

    mysql知识点

---
+++ 前端学习/html学习之服务器.py
@@ -0,0 +1,14 @@
+import socket
+
+sk = socket.socket()
+sk.bind(('127.0.0.1', 9000))
+sk.listen(1)
+conn,addr = sk.accept()
+ret = conn.recv(1024)
+print(ret.decode('utf-8'))
+conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
+with open('socket_html.html',mode='rb') as f:
+    data = f.read()
+conn.send(data)
+conn.close()
+sk.close()
